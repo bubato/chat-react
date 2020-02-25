75108a4c49e8fc544e028e11a2875293e0942773
HoangTV <hoangtv@gemvietnam.com>
2020-02-25 16:03:05 +0700

    CR-Messenger-Update-1

--- src/App.js
+++ src/App.js
@@ -4,11 +4,9 @@ import { Route, Switch, Redirect, Router } from 'react-router-dom'
 import './App.css';
 import GroupChat from './layout_body/GroupChat';
 import MessengerAction from './layout_body/MessengerAction';
-<<<<<<< HEAD
 import axios from 'axios';
-=======
 import Login from './layout_body/Login'
->>>>>>> Create Login Form
+import Cookies from 'js-cookie'
 
 axios.defaults.baseURL = "http://muotphim.top/";
 function App() {
@@ -17,8 +15,6 @@ function App() {
       <div className="messaging">
         <h1 className="text-center my-4">Skope</h1>
         <div className="inbox_msg">
-
-          {/* <Login/> */}
           <Switch>
             <Redirect exact from="/" to="/login" />
             <Route path="/login" component={Login} />
@@ -29,8 +25,6 @@ function App() {
               </div>
             } />
           </Switch>
-          {/* <GroupChat/>
-          <MessengerAction/> */}
         </div>
       </div>
     </div>
