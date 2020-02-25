fe51ad7a8e8bc3c1d222cd6458162185d14141fa
HoangTV <hoangtv@gemvietnam.com>
2020-02-25 17:23:57 +0700

    CR-Messenger-Update-2

--- src/layout_body/messenger_item/OtherMessenger.js
+++ src/layout_body/messenger_item/OtherMessenger.js
@@ -1,18 +1,34 @@
 import React from 'react';
+import axios from 'axios';
 
 class OtherMessenger extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      Data: this.props.value
+      Data: this.props.value,
+      Avatar: null
     }
+  }
+  componentDidMount() {
+    axios.get("account/" + this.props.value.id_account)
+    .then((response) => {
+        console.log("Response OKK")
+        console.log(response.data);
+        this.setState({
+          Avatar: response.data.avatar
+        })
+    }) 
+    .catch((error) => {
+        console.log("Response Error")
+        console.log(error);
+    });
   }
   render() {
     let RenderMessenger = this.state.Data
     return (
       <div className="incoming_msg">
-        <div className="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"/> </div>
+        <div className="incoming_msg_img"> <img src={this.state.Avatar} title={RenderMessenger.name}/> </div>
         <div className="received_msg">
           <div className="received_withd_msg">
             <p>{RenderMessenger.content}</p>
